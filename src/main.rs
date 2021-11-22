219c9686586fb85f6340eb446b300d4fb59614b0
lennert.commeine <lennert.commeine@student.howest.be>
2021-11-22 10:55:55 +0100

    queue message reader

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
-use warp::Filter;
+// Port of https://www.rabbitmq.com/tutorials/tutorial-one-python.html. Run this
+// in one shell, and run the hello_world_publish example in another.
+use amiquip::{Connection, ConsumerMessage, ConsumerOptions, QueueDeclareOptions, Result};
 
-#[tokio::main]
-async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, Humam, welcome to {}!", name));
+fn main() -> Result<()> {
+    // Open connection.
+    let mut connection = Connection::insecure_open("amqp://guest:guest@localhost:5672")?;
 
-    warp::serve(hello)
-        .run(([0, 0, 0, 0], 3030))
-        .await;
+    // Open a channel - None says let the library choose the channel ID.
+    let channel = connection.open_channel(None)?;
+
+    // Declare the "hello" queue.
+    let queue = channel.queue_declare("hello", QueueDeclareOptions::default())?;
+
+    // Start a consumer.
+    let consumer = queue.consume(ConsumerOptions::default())?;
+    println!("Waiting for messages. Press Ctrl-C to exit.");
+
+    for (i, message) in consumer.receiver().iter().enumerate() {
+        match message {
+            ConsumerMessage::Delivery(delivery) => {
+                let body = String::from_utf8_lossy(&delivery.body);
+                println!("({:>3}) Received [{}]", i, body);
+                consumer.ack(delivery)?;
+            }
+            other => {
+                println!("Consumer ended: {:?}", other);
+                break;
+            }
+        }
+    }
+
+    connection.close()
 }
+
